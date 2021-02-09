26a3070f756941705283162b101446a99e912ab4
Cavalo Tycho <ilnur_gumerov_1996@mail.ru>
2021-02-09 10:03:34 +0300

    09.02.2021

---
+++ src/ft_strcmp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ctycho <ctycho@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/02/09 09:51:44 by ctycho            #+#    #+#             */
+/*   Updated: 2021/02/09 09:52:40 by ctycho           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int		ft_strcmp(const char *s1, const char *s2)
+{
+	size_t	i;
+	int		res;
+
+	i = 0;
+	res = 0;
+	while (s1[i] || s2[i])
+	{
+		if (s1[i] < s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		if (s1[i] > s2[i])
+			return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+		i++;
+	}
+	return ((unsigned char)res);
+}
