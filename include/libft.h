160ad55ba646266be2eea856aabf426a20c1813d
Cavalo Tycho <ilnur_gumerov_1996@mail.ru>
2021-02-11 15:50:47 +0300

    add memdel

--- include/libft.h
+++ include/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ctycho <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: ctycho <ctycho@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/31 13:26:54 by ctycho            #+#    #+#             */
-/*   Updated: 2020/11/10 19:05:26 by ctycho           ###   ########.fr       */
+/*   Updated: 2021/02/11 15:49:45 by ctycho           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ typedef struct		s_list
 
 void				*ft_memset(void *str, int c, size_t len);
 void				ft_bzero(void *s, size_t n);
+void				*ft_memdel(void *ptr);
 void				*ft_memcpy(void *dst, const void *src, size_t n);
 void				*ft_memccpy(void *dest, const void *src, int c, size_t n);
 void				*ft_memmove(void *dst, const void *src, size_t len);
